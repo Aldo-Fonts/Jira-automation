e1ae3bf422af4a9dade9e1649e03752935fe9e6c
Aldo Fontes <aldo.fonts23@gmail.com>
2023-07-11 14:18:36 -0600

    This is the begining
    These are the files to start

---
+++ Packages/login.py
@@ -0,0 +1,17 @@
+#####################################################################################################################################################################################
+# File:         login.py
+# Version:      1.0
+# Author:       Aldo_Fonts
+# Description:  The current file has the functions and the tools to login on Jira
+#####################################################################################################################################################################################
+# Function(s)
+def getCredentials():
+    return
+
+def putCredentials():
+    return
+
+def saveCredentials(remember):
+    if(remember == True):
+        # Save in a .txt
+    return
